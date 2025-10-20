b6cd887d4086c75b8b09bac70ba6ff7433c815b1
amirQuf <amir.ghasemian.ag@gmail.com>
2025-10-20 12:35:32 +0330

    writing task model

---
+++ backend/tasks/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from team.models.project import Project
+from users.models import CustomUser
+from django.utils import timezone
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = "L"
+        MEDIUM = "M"
+        HIGHT = "H"
+        URGENT = "U"
+
+    class Status(models.TextChoices):
+        TODO = "T"
+        IN_PROGRESS = "P"
+        DONE = "D"
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    description = models.TextField(null=True, blank=True)
+    assigned_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    priority = models.CharField(
+        max_length=1, choices=Priority.choices, default=Priority.LOW
+    )
+    status = models.CharField(max_length=1, choices=Status.choices, default=Status.TODO)
+
+    due_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-updated_at", "-created_at"]
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.title
